7bd847eaf194b0d55afe526e3a4ad30a285aa44d
Utkarsh Yadav <yadavutkarsh207@gmail.com>
2020-05-03 20:30:09 +0530

    Add Keep

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        Note Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
