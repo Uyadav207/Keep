cb69c98afab1a5363840ed7bb933b4db738d7c13
Uyadav207 <yadavutkarsh207@gmail.com>
2020-09-06 14:55:46 +0530

    Deploy Fix Made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
@@ -36,7 +35,7 @@ function App() {
           />
         );
       })}
-      {/* <Footer /> */}
+  
       </main>
 
     </div>
